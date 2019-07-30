0d70eae013678a1bdb27aacc0b4f5a3fca45d8ec
arturolei <artie.lei@gmail.com>
2019-07-29 22:36:42 -0700

    Almost MVP complete but not quite. POST for posts not quite working

--- users/userRouter.js
+++ users/userRouter.js
@@ -1,15 +1,27 @@
 const express = require('express');
 const users = require('./userDb.js');
+const posts = require('../posts/postDb.js')
 const router = express.Router();
 
+
 //POST--Create USER
-router.post('/', async (req, res) => {
-    
+router.post('/', validateUser, async (req, res) => {
+    try {
+        const newUser = await users.insert(req.body);
+        res.status(201).json(newUser);
+    } catch (error){
+        res.status(500).json({message: error});
+    }
 });
 
 //POST--Create a post for USER by ID
-router.post('/:id/posts', async (req, res) => {
-
+router.post('/:id/posts', validateUserId,validatePost, async (req, res) => {
+    try{
+        const newPost = await posts.insert(req.body);
+        res.status(201).json(comment);
+    } catch (error) {
+        res.status(500).json(error);
+    }
 });
 
 //GET
@@ -55,8 +67,9 @@ router.delete('/:id', validateUserId, async (req, res) => {
     }
 });
 
-router.put('/:id', validateUserId, async (req, res) => {
-
+router.put('/:id', validateUserId,validateUser, async (req, res) => {
+    const updatedUser = await users.update(req.params.id, req.body);
+    res.status(200).json(req.body);
 });
 
 //custom middleware
@@ -77,11 +90,37 @@ async function validateUserId(req, res, next) {
 };
 
 function validateUser(req, res, next) {
-
+    try {
+        if (req.body && Object.keys(req.body).length > 0){
+            if (req.body.name){
+                next();
+            } else {
+                res.status(400).json({message:"missing required name field"})
+            }
+        } else {
+            res.status(400).json({ message: "missing user data" });
+        }
+    }
+    catch(error){
+        res.status(500).json(error);
+    }
 };
 
 function validatePost(req, res, next) {
-
+    try {
+        if (req.body && Object.keys(req.body).length > 0){
+            if (req.body.text && req.body.user_id){
+                next();
+            } else {
+                res.status(400).json({message:"missing required text field"})
+            }
+        } else {
+            res.status(400).json({ message: "missing post data" });
+        }
+    }
+    catch(error){
+        res.status(500).json(error);
+    }
 };
 
 module.exports = router;
